96a8e1b1128dd9cc899e09b1e48c77c452498442
Isura Edirisinghe <isurae@gmail.com>
2013-11-02 18:14:12 -0400

    Add data folder and csv data.

---
+++ data/import.sql
@@ -0,0 +1,8 @@
+.separator ","
+.import first_name.csv player_firstname
+.import last_name.csv player_lastname
+.import region.csv player_region
+.import state.csv player_state
+.import position.csv player_position
+.import talent_profile.csv player_talentprofile
+.import profile_point.csv player_profilepoint
