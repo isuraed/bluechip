66944f3084a48e4b2122caac0e24e5a54c10aff1
Isura Edirisinghe <isurae@gmail.com>
2013-11-03 20:18:44 -0500

    Enable static content and static context processor.

--- bluechip/bluechip/settings.py
+++ bluechip/bluechip/settings.py
@@ -114,13 +114,17 @@ TEMPLATE_DIRS = (
     '/home/isura/Devel/bluechip/bluechip/templates',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.static',
+)
+
 INSTALLED_APPS = (
     #'django.contrib.auth',
     #'django.contrib.contenttypes',
     #'django.contrib.sessions',
     #'django.contrib.sites',
     #'django.contrib.messages',
-    #'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
