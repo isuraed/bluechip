c813da521605c1b5df73d13f89fcea9027cc7027
Isura Edirisinghe <isurae@gmail.com>
2013-11-07 21:24:57 -0500

    Add basic player app homepage with navigation links and views to filter players.

--- bluechip/player/urls.py
+++ bluechip/player/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from player import views
 
 urlpatterns = patterns('',
-  url(r'^all/$', views.all),
-  url(r'^top-100/$', views.top_100),
-  url(r'^qb/$', views.qb),
+	url(r'^$', views.home),
+	url(r'^all/$', views.all),
+  	url(r'^top-100/$', views.top_100),
 )
--- bluechip/player/views.py
+++ bluechip/player/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from models import Player
 
+def home(request):
+	return render(request, 'player/home.html')
+
 
 def all(request):
-	players_list = Player.objects.all()
-	return render(request, 'players.html', {'players_list': players_list})
+	players_list = Player.objects.all().order_by('-grade')
+	return render(request, 'player/table.html', {'players_list': players_list})
 
 
 def top_100(request):
 	players_list = Player.objects.all().order_by('-grade')[:100]
-	return render(request, 'players.html', {'players_list': players_list})
+	return render(request, 'player/table.html', {'players_list': players_list})
 
 
-def qb(request):
-	players_list = Player.objects.filter(position__name__contains="QB")
-	return render(request, 'players.html', {'players_list': players_list})
+# def qb(request):
+# 	players_list = Player.objects.filter(position__name__contains="QB")
+# 	return render(request, 'players.html', {'players_list': players_list})
